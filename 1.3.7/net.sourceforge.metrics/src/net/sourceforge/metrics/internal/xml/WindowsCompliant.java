4151df426c587128d4fa8988110ea68135d38be9
gboissier <gboissier@d51ba4a3-7f96-4e61-b9d4-3571a4535bf5>
2009-10-21 21:53:25 +0000

    moved

--- 1.3.7/net.sourceforge.metrics/src/net/sourceforge/metrics/internal/xml/WindowsCompliant.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.metrics.internal.xml;
-
-public class WindowsCompliant extends MetricsFirstExporter {
-	
-	protected String formatXMLStr(String handle) {
-		return handle.replaceAll("<", "[@").replaceAll(">", "@]").replaceAll("\\\\", "_");
-	}
-	
-}
