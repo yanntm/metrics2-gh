a384ec0bcbb3c331679f9f50666d41d7628c9ae9
kcassell <kcassell@d51ba4a3-7f96-4e61-b9d4-3571a4535bf5>
2010-01-19 21:37:39 +0000

    Fixed bug.

--- activity/1.4.x_newMetricComputations/net.sourceforge.metrics/src/net/sourceforge/metrics/calculators/LCOMHS.java
+++ activity/1.4.x_newMetricComputations/net.sourceforge.metrics/src/net/sourceforge/metrics/calculators/LCOMHS.java
@@ -19,6 +19,7 @@
  */
 package net.sourceforge.metrics.calculators;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -65,19 +66,14 @@ public class LCOMHS extends CohesionCalculator {
 	CallData callData = typeSource.getCallData();
 	Set<IField> attributes = callData.getAttributes();
 	Set<IMethod> methods = callData.getMethods();
-	Map<IField, HashSet<IMethod>> attributeAccessedByMap =
-	    callData.getAttributeAccessedByMap();
-
 	double value = 0;
 
 	// TODO check attributes > 1 with paper.  The book does not
 	// mention what to do with 0-1 attributes/methods, but references
 	// another Henderson-Sellers paper which is not easily obtainable.
 	if ((attributes.size() > 1) && (methods.size() > 1)) {
-	    if (attributeAccessedByMap.size() > 0) {
 		// TODO boolean countStatics = getPrefs().countStaticMethods();
-		value = calculateResult(attributeAccessedByMap);
-	    }
+		value = calculateResult(callData);
 	}
 	System.out.println("Setting HS to " + value + " for "
 		+ source.getName());
@@ -88,23 +84,29 @@ public class LCOMHS extends CohesionCalculator {
      * @return double (avg(m(a)) - m)/(1 - m) where m(a) is the number of
      *         methods that access a
      */
-    private double calculateResult(
-	    Map<IField, HashSet<IMethod>> attributeAccessedByMap) {
-	int sum = 0;
-	int numAttributes = 0;
-	Set<IMethod> allMethods = new HashSet<IMethod>();
-	// TODO remove redundant calculation
-	for (Iterator<HashSet<IMethod>> i = attributeAccessedByMap.values().iterator();
-		i.hasNext(); numAttributes++) {
-	    Set<IMethod> methods = (Set<IMethod>) i.next();
-	    allMethods.addAll(methods);
-	    sum += methods.size();
+    private double calculateResult(CallData callData) {
+	double result = 0; // maximally cohesive
+	Set<IMethod> allMethods = callData.getMethods();
+	
+	if (allMethods != null) {
+	    int numMethods = allMethods.size();
+	    
+	    if (numMethods > 1) {
+		int sum = 0;
+		Map<IField, HashSet<IMethod>> attributeAccessedByMap =
+		    callData.getAttributeAccessedByMap();
+		Collection<HashSet<IMethod>> values = attributeAccessedByMap.values();
+		int numAttributes = values.size();
+		Iterator<HashSet<IMethod>> valueIterator = values.iterator();
+		
+		while (valueIterator.hasNext()) {
+		    Set<IMethod> methods = (Set<IMethod>) valueIterator.next();
+		    sum += methods.size();
+		}
+		double avg = (double) sum / (double) numAttributes;
+		result = Math.abs((avg - numMethods) / (1 - numMethods)); // avoids -0.0
+	    }
 	}
-	int numMethods = allMethods.size();
-	if (numMethods == 1)
-	    return 0;
-	double avg = (double) sum / (double) numAttributes;
-	double result = Math.abs((avg - numMethods) / (1 - numMethods));
 	return result;
     }
 
