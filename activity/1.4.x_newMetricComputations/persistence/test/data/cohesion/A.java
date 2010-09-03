5184cb760e6d7b14470724937b841ca50cdf56e6
gboissier <gboissier@d51ba4a3-7f96-4e61-b9d4-3571a4535bf5>
2010-09-03 21:12:06 +0000

    cleaning before merging and setting up branches back ...

--- activity/1.4.x_newMetricComputations/persistence/test/data/cohesion/A.java
+++
@@ -1,6 +0,0 @@
-package data.cohesion;
-
-public class A
-{
-    protected int a = 0;
-}
--- activity/1.4.x_newMetricComputations/persistence/test/data/cohesion/MA.java
+++
@@ -1,8 +0,0 @@
-package data.cohesion;
-
-public class MA
-{
-    protected int a = 0;
-    public int getA() { return a; }
-    public void setA(int a) { this.a = a; }
-}
