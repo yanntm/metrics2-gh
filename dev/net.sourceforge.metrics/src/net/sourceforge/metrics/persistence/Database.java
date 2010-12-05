ec2bc0638bdd9ecbdd242af363dc2bdeed676f6a
kcassell <kcassell@d51ba4a3-7f96-4e61-b9d4-3571a4535bf5>
2010-12-05 22:36:52 +0000

    Added copyright notice.

--- dev/net.sourceforge.metrics/src/net/sourceforge/metrics/persistence/Database.java
+++ dev/net.sourceforge.metrics/src/net/sourceforge/metrics/persistence/Database.java
@@ -1,3 +1,30 @@
+/*
+ * Some of this class is based on code obtained from the Apache Derby project
+ * and modified.  The original Apache header with the licensing information is
+ * included below.
+ */
+
+/*
+
+   Derby - Class SimpleApp
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
 package net.sourceforge.metrics.persistence;
 
 import java.sql.Connection;
